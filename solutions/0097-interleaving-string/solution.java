7241ae8c427ddc45d40a267bea70659000568140
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2025-09-22 10:45:03 +0000

    Sync LeetCode submission Runtime - 3 ms (71.13%), Memory - 41.8 MB (90.27%)

---
+++ solutions/0097-interleaving-string/solution.java
@@ -0,0 +1,69 @@
+// class Solution {
+//     public boolean isInterleave(String s1, String s2, String s3) {
+//         if(s1.length()+s2.length()!=s3.length()) return false;
+//         for(int i=0;i<s2.length();i++)
+//         {
+//             for(int j=0;j<s3.length();j++)
+//             {
+//                 if(s2.charAt(i)==s3.charAt(j))
+//                 {
+//                    StringBuilder sb = new StringBuilder(s3);
+//                    sb.setCharAt(j, '#');  
+//                    s3 = sb.toString(); 
+//                 }
+//             }
+//         }
+//         for(int i=0;i<s1.length();i++)
+//         {
+//             for(int j=0;j<s3.length();j++)
+//             {
+//                 if(s1.charAt(i)==s3.charAt(j))
+//                 {
+//                     StringBuilder sb1 = new StringBuilder(s3);
+//                     sb1.setCharAt(j, '#'); 
+//                     s3 = sb1.toString();   
+
+//                 }
+//             }
+//         }
+//         for(int i=0;i<s3.length();i++)
+//         {
+//             if(s3.charAt(i) != '#')
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) return false;
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+
+        
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+   
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                         || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+
