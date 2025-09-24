e7e00d5b548ae3e2855c64fc51a53874df8fe013
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2025-09-24 05:32:07 +0000

    Sync LeetCode submission Runtime - 7 ms (20.34%), Memory - 43 MB (74.93%)

---
+++ solutions/0257-binary-tree-paths/solution.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if(root == null) return res;
+
+        dfs(root, "",res);
+        return res;
+    }
+
+    // private void dfs(TreeNode node, String path, List<String> res){
+    //     if(node == null) return;
+
+    //     if(path.isEmpty()){
+    //         path = "" + node.val;
+    //     }else{
+    //         path = path + "->" + node.val;
+    //     }
+
+    //     if(node.left == null && node.right == null){
+    //         res.add(path);
+    //         return;
+    //     }
+
+    //     dfs(node.left,path,res);
+    //     dfs(node.right,path,res);
+    // }
+    private void dfs(TreeNode node,String path,List<String> res)
+    {
+        if(node==null) return;
+        path += node.val;
+        if(node.left == null && node.right == null)
+        {
+            res.add(path);
+        }
+        else
+        {
+            path += "->";
+            if(node.left != null) dfs(node.left,path,res);
+            if(node.right != null) dfs(node.right,path,res);
+        }
+
+
+
+    }
+}
