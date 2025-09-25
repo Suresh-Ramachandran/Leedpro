7d8ab944874c5546ed09723cff1230039a59be2b
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2025-09-25 03:00:29 +0000

    Sync LeetCode submission Runtime - 1 ms (99.76%), Memory - 44.7 MB (95.68%)

---
+++ solutions/0383-ransom-note/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+
+        for(char i:magazine.toCharArray())
+        {
+            arr[i-'a']++;
+        }
+        for(char i:ransomNote.toCharArray())
+        {
+            if(arr[i-'a']==0)
+            {
+                return false;
+            }
+            arr[i-'a']--;
+        }
+        return true;
+    }
+}
