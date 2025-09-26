0a61ea0ca1b0a60613f2e8541c2f48de7c923ed4
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2025-09-26 05:17:38 +0000

    Sync LeetCode submission Runtime - 2 ms (45.00%), Memory - 43.3 MB (30.00%)

---
+++ solutions/1944-truncate-sentence/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] str = s.split(" ");
+        String res="";
+        for(int i=0;i<k;i++)
+        {
+            res = res+str[i];
+            if(i!=k-1)
+            {
+                res += " ";
+            }
+        }
+        return res;
+    }
+}
