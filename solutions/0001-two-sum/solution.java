078103b04c0dbfb81e13f1f643a7991216aff375
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2025-12-10 15:56:01 +0000

    Sync LeetCode submission Runtime - 2 ms (99.10%), Memory - 47.3 MB (11.31%)

--- solutions/0001-two-sum/solution.java
+++ solutions/0001-two-sum/solution.java
@@ -1,47 +1,18 @@
-import java.util.*;
-
 class Solution {
-    public int[] twoSum(int[] list, int target) {
-        int n = list.length; // Define `n`
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (list[i] + list[j] == target) {
-                    return new int[]{i, j}; // Return indices instead of printing
-                }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[]{ map.get(complement), i };
             }
+            
+            map.put(nums[i], i);
         }
-        return new int[]{}; // Return an empty array if no solution is found
-    }
-
-    public static void main(String[] args) { // `String` instead of `str`
-        Scanner sc = new Scanner(System.in);
         
-        // Read array size
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
-        
-        int[] list = new int[n]; // Use an array instead of ArrayList
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            list[i] = sc.nextInt();
-        }
-
-        // Read target value
-        System.out.print("Enter target sum: ");
-        int target = sc.nextInt();
-
-        // Call the function
-        Solution obj = new Solution();
-        int[] result = obj.twoSum(list, target);
-
-        // Print result
-        if (result.length > 0) {
-            System.out.println("[" + result[0] + ", " + result[1] + "]");
-        } else {
-            System.out.println("No valid pair found.");
-        }
-
-        sc.close();
+        return new int[]{};
     }
 }
 
