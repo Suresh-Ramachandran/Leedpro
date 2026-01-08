c89f3d1a4dffe95efc96c4722299e05fa506bedb
Suresh <117349539+Suresh-Ramachandran@users.noreply.github.com>
2026-01-08 04:21:55 +0000

    Sync LeetCode submission Runtime - 7 ms (75.40%), Memory - 49.8 MB (41.55%)

---
+++ solutions/0049-group-anagrams/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+
+        for(String s : strs)
+        {
+            char[] ch1 = s.toCharArray();
+            Arrays.sort(ch1);
+            String s1 = String.valueOf(ch1);
+
+
+            if(!map.containsKey(s1))
+            {
+                map.put(s1,new ArrayList<>());
+            }
+            map.get(s1).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
